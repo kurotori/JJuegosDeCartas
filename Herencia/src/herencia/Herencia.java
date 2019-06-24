8dc8cc316ee9c26166f43a460575d6466563a20b
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2019-06-24 16:52:58 -0300

    Actualizacion de código

---
+++ Herencia/src/herencia/Herencia.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Herencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventanita v1 = new Ventanita();
+        v1.setVisible(true);
+        
+        Ventana v2 = new Ventanita();
+    }
+    
+}
