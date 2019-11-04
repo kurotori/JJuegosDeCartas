5c75885aeff02465573645f8ee783215cca9d9df
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2019-11-04 16:29:23 -0300

    Creación de evaluación final

---
+++ Evaluacionfinal/src/evaluacionfinal/Evaluacionfinal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evaluacionfinal;
+
+/**
+ *
+ * @author luiss
+ */
+public class Evaluacionfinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuPrincipal v = new MenuPrincipal();
+        v.setVisible(_______);
+    }
+    
+}
---
+++ Evaluacionfinal/src/evaluacionfinal/VentanaConMensaje.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evaluacionfinal;
+
+/**
+ *
+ * @author luiss
+ */
+public class VentanaConMensaje extends VentanaBase{
+    
+    public VentanaConMensaje(String __________ ){
+        CambiarMensaje(mensaje);
+    }
+}
